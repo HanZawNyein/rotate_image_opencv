f786a95c74785e380d6d2099f77eb444c10ae4f4
HanZawNyein <hanzawnyineonline@gmail.com>
2022-11-08 23:08:42 +0630

    greyscale and invert

--- greyscale_image.py
+++ greyscale_image.py
@@ -8,9 +8,11 @@ src = cv2.imread(path)
 # Convert to Grayscale
 img_gray = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
 
+# invert
+invert_img = cv2.bitwise_not(img_gray)
 # To display Image
 window_name='Grayscale Conversion OpenCV'
 cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-cv2.imshow(window_name,img_gray)
+cv2.imshow(window_name,invert_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
