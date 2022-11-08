6241302d5dddff8f1c0a6669ba78c3f1da0a84b0
HanZawNyein <hanzawnyineonline@gmail.com>
2022-11-08 22:26:16 +0630

    flip image

---
+++ flip_image.py
@@ -0,0 +1,19 @@
+# importing cv2
+import cv2
+
+# path
+path = r"images\download.jpg"
+
+# Reading an image in default mode
+src = cv2.imread(path)
+
+# Window name in which image is displayed
+window_name = 'Image'
+
+# Using cv2.flip() method
+# Use Flip code 0 to flip vertically
+image = cv2.flip(src, 0)
+
+# Displaying the image
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
